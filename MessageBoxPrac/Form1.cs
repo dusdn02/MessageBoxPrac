f30966b74769b6c980b3279ef4610144635f6936
dusdn02 <lyw9114@naver.com>
2020-06-16 10:41:13 +0900

    초기 커밋.

---
+++ MessageBoxPrac/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageBoxPrac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("내용");
+            MessageBox.Show("내용","제목");
+            MessageBox.Show("내용","제목",MessageBoxButtons.RetryCancel);
+            MessageBox.Show("내용","제목",MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Asterisk);
+        }
+    }
+}
